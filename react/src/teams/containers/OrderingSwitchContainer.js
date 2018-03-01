29e94d8ef546e8ec2e6a560e110ad7851a7100e2
Lantic <lanticgan@outlook.com>
2018-03-01 11:20:32 +0800

    Update the directory

---
+++ react/src/teams/containers/OrderingSwitchContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import TeamsStore from '../stores/TeamsStore';
+import '../style/OrderingSwitch.less';
+
+@observer
+class OrderingSwitchContainer extends React.Component {
+  renderOrderingSwitch = () => {
+    return (
+      <div>
+        <div 
+          className={
+          `${TeamsStore.ordering === 'hot' ? 'hot-selected' : 'hot'}`}
+          // compatibility
+          onClick={() => { TeamsStore.switchOrdering('hot'); }}
+          onKeyUp={() => { TeamsStore.switchOrdering('hot'); }}
+        />
+        <div 
+          className={
+          `${TeamsStore.ordering === 'time' ? 'time-selected' : 'time'}`}
+          // compatibility
+          onClick={() => { TeamsStore.switchOrdering('time'); }}
+          onKeyUp={() => { TeamsStore.switchOrdering('time'); }}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="tab-wrapper">
+        {this.renderOrderingSwitch()}
+      </div>
+    );
+  }
+}
+
+export default OrderingSwitchContainer;
