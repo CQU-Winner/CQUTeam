cecf01e70c26cd3bfe0dbcbee258e6281cdda741
Lantic <lanticgan@outlook.com>
2018-03-07 23:26:04 +0800

    Update postpage

--- react/src/shared/PostPage/PostPage.js
+++ react/src/shared/PostPage/PostPage.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { List, InputItem } from 'antd-mobile';
+import { createForm } from 'rc-form';
 import TopNavBar from '../NavBar/TopNavBar';
 
 class PostPage extends React.Component {
+  submit = () => {
+    this.props.form.validateFields((error, value) => {
+      console.log(error, value);
+    });
+  }
+  
   render() {
+    const { getFieldProps } = this.props.form;
     return (
       <div>
         <TopNavBar title="需求发布" />
         <List renderHeader={() => '需求详情'}>
           <InputItem
-            placeholder="please input content"
-            data-seed="logId"
+            {...getFieldProps('required')}
+            clear
+            placeholder="auto focus"
           >
-            非受控组件
+            标题
           </InputItem>
+          <InputItem
+            {...getFieldProps('digit')}
+            clear
+            placeholder="click the button below to focus"
+          >
+            标题
+          </InputItem>
+          <button onClick={this.submit}>submit</button>
         </List>
       </div>
     );
   }
 }
 
-export default PostPage;
+export default createForm()(PostPage);
