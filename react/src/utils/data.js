2ddf8047bb333f90c2099ee5bcd1c2460fb7b983
ganyuting <lanticgan@outlook.com>
2018-04-20 11:06:42 +0800

    Add more competType

--- react/src/utils/data.js
+++ react/src/utils/data.js
@@ -7,6 +7,34 @@ const competitionType = [
     label: '应用开发',
     value: '应用开发',
   },
+  {
+    label: '创业大赛',
+    value: '创业大赛',
+  },
+  {
+    label: '学科学术',
+    value: '学科学术',
+  },
+  {
+    label: '科技大赛',
+    value: '科技大赛',
+  },
+  {
+    label: '公益大赛',
+    value: '公益大赛',
+  },
+  {
+    label: '体育竞技',
+    value: '体育竞技',
+  },
+  {
+    label: '游戏竞技',
+    value: '游戏竞技',
+  },
+  {
+    label: '其他比赛',
+    value: '其他比赛',
+  },
 ];
 
 export { competitionType };
